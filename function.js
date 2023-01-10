4eecf1ae304f20e15717c42740ad6f43782972a7
Omar8912 <122367101+Omar8912@users.noreply.github.com>
2023-01-10 15:57:55 +0100

    Update function.js

--- function.js
+++ function.js
@@ -2,7 +2,14 @@ export function feladat01(szinek){
 
 	let darab = szinek.length; //a tömb mérete számban
     
-    let valasz = 2**darab-1-darab //a csoportosíthatóságot a binomiális tétel segítségével (2^n)-ként határoztam meg, ahol n a teljes halmaz mérete; ebből az üres halmaz mérete (-1) és az egy elemű halmazok mérete (-darab) levonásra kerül
+    let valasz; //a lehetséges színkombinációk száma
+
+if(darab > 1){
+    valasz = 2**darab-1-darab; //a csoportosíthatóságot a binomiális tétel segítségével (2^n)-ként határoztam meg, ahol n a teljes halmaz mérete; ebből az üres halmaz mérete (-1) és az egy elemű halmazok mérete (darab) levonásra kerül
+    }
+    else{
+    valasz = 0;//válasz, amennyiben kevesebb, mint két szín került megadásra
+    }
     
 return valasz;
 }; 
