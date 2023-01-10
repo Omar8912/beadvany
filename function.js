dc0ec9169a322d81c2cd68e6829dda68359a1cbb
Omar8912 <122367101+Omar8912@users.noreply.github.com>
2023-01-10 15:46:39 +0100

    Create function.js

---
+++ function.js
@@ -0,0 +1,8 @@
+export function feladat01(szinek){
+
+	let darab = szinek.length; //a tömb mérete számban
+    
+    let valasz = 2**darab-1-darab //a csoportosíthatóságot a binomiális tétel segítségével (2^n)-ként határoztam meg, ahol n a teljes halmaz mérete; ebből az üres halmaz mérete (-1) és az egy elemű halmazok mérete (darab) levonásra kerül
+    
+return valasz;
+}; 
